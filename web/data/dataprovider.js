a87d77343ae11d9748d28dd23a50f81d24187b1e
Carlos <camoru@gmail.com>
2024-11-14 19:43:16 +0100

    inicio y login "terminados"

---
+++ web/data/dataprovider.js
@@ -0,0 +1,8 @@
+//meter los .json
+var peliculas = require();
+var usuarios = require();
+
+//Obtener todas las pelis// DA FALLO ESTO
+function getAllPeliculas() {
+    return peliculas;
+}
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/*var datos = require("../data/dataprovider");
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const peliculas = datos.getAllPeliculas(); 
+  res.render('home', {title: "Inicio", peliculas: peliculas});
+});*/
+
+//para borrar este de abajo cuando termine, es testing
+router.get('/', function(req, res, next) {   
+  res.render('home', {title: "Inicio"});
 });
+
+router.get('/login', function(req, res) {
+  res.render('login', { title: "Login" });
+});
+
 
 module.exports = router;
